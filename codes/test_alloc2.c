910610849326454ca0d1596cf9b759e1d19d338d
Emery Assogba <assogba.emery@gmail.com>
2022-03-09 11:50:07 +0100

    comments for test_alloc2

--- codes/test_alloc2.c
+++ codes/test_alloc2.c
@@ -4,9 +4,29 @@ struct node{
   struct node *next;
 };
 
+/** 
+   Initialise une liste vide
+**/
 struct node *init_node(void);
-struct node *addHead(struct node *head);
-void addTail(struct node *head);
+
+/** Ajoute la valeur à la tête de la liste et retourne 
+ *  la nouvelle tête (head)
+ **/
+struct node *addHead(struct node *head, int val);
+
+/** Ajoute l'élément à la fin de la liste. 
+ * La tête de liste ne change pas
+ **/
+void addTail(struct node *head, int val);
+
+/** 
+ * alloue de la mémoire pour un nouveau noeud et retourne ce noeud
+ **/
+struct node *new_node(int val);
+
+/** affiche tous les éléments de la liste 
+ *  au format val: <valeur>
+ **/
 void display_list(struct node *head);
 
 int main(int argc, char *argv[]){
