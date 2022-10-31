f2ec9307f13b0db3009396ac22440f2c6e8c0590
Emery Assogba <assogba.emery@gmail.com>
2022-10-31 13:12:38 +0100

    add async io

---
+++ codes/test_io.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]){
+   int ret, nchars = 0;
+   // configuration de la sortie standard en non bloquant
+   fcntl(1, F_SETFL, O_NONBLOCK );
+   
+   // Debut de l'écriture
+   while(1){
+       ret = write(1, "Bonjour  ", 10 );
+       if(ret < 0){
+          if(errno == EAGAIN){
+             printf("Buffer is full: %d\n", nchars);
+             break;
+          }
+       }
+       nchars+=ret;
+   }
+}
